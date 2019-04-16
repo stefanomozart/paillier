6bee12477d00177211f22411ec13184b24c5cbb5
stefanomozart <stefanomozart@sporos.com.br>
2019-04-16 19:04:30 -0300

    improving documentation

--- optimizations.go
+++ optimizations.go
@@ -9,6 +9,9 @@ func StandardGroupParams(bits int64) (*big.Int, *big.Int) {
 	return nil, nil
 }
 
+// NewKeyPairWithSGP generates a Paillier key pair using standard pre-computed
+// group paramaters. It has only testing purpouses. Do not use it for
+// real data encryption
 func NewKeyPairWithSGP(bits int64) (*PrivateKey, *PublicKey) {
 	n, g := StandardGroupParams(bits)
 
@@ -37,12 +40,11 @@ func (pk *PublicKey) BatchAdd(cts ...*big.Int) *big.Int {
 // is fixed as `g = n+1`, the regular encryption `g^msg * r^n mod n^2` can be
 // optmized to `(n+1)*(m+1)*r^n mod n^2`, requiring one less modular exponentiation
 func (pk *PublicKey) EncryptWithWithSGP(msg int64) *big.Int {
-
-	total := new(big.Int).SetInt64(1)
-	for _, ct := range cts {
-		total.Mul(total, ct)
-	}
-	return total.Mod(total, pk.N2)
+	m := new(big.Int).SetInt64(msg + 1)
+	r := new(big.Int).Exp(getRandom(pk.N), pk.N, pk.N2)
+	m.Mul(new(big.Int).Add(pk.N, one), m)
+	c := new(big.Int).Mul(m, r)
+	return c.Mod(c, pk.N2)
 }
 
 func split(sum int) (x, y int) {
